09a9efeb0669b5bf6f54e7db3645eaa272cf1509
Edu58 <edumuriithi58@gmail.com>
2022-05-02 00:06:50 +0300

    adds test for NewsSource model

---
+++ tests/test_newssource_model.py
@@ -0,0 +1,14 @@
+import unittest
+from models import NewsSource
+
+
+class TestNewsSource(unittest.TestCase):
+    def setUp(self) -> None:
+        self.news = NewsSource('kbc', 'kenya broadcasting corporation', 'a government owned media house')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.news, NewsSource))
+
+
+if __name__ == '__main__':
+    unittest.main()
